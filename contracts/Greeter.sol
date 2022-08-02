70e60c97283ef1549d3c54604c45c1d7d6863db5
tahlil <bsse0803@iit.du.ac.bd>
2022-08-02 16:17:48 +0600

    Retun owner

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -26,4 +26,8 @@ contract Greeter {
         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
         greeting = _greeting;
     }
+
+    function getOwner() public view returns(address) {
+        return _owner;
+    }
 }
